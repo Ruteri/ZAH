78ed100a3d49b3feeec0a1ee6f2c36de33cdb25a
akowalew <ram.techen@gmail.com>
2018-01-16 18:31:33 +0100

    Updated cli to use proper declaration of Model.use

--- app/cli.py
+++ app/cli.py
@@ -3,15 +3,6 @@
 import sys
 from model.model import Model
 
-def rejected():
-    print "rejected"
-
-def notified():
-    print "notified"
-
-def done():
-    print "done"
-
 def main():
     if len(sys.argv) < 2:
         print('Usage: {0} <data_dir>'.format(sys.argv[0]))
@@ -19,7 +10,7 @@ def main():
 
     dataDirectory = sys.argv[1]
     model = Model(dataDirectory)
-    model.run(done, notified, rejected)
+    model.run()
 
 if __name__ == "__main__":
     main()
