1ff611f1e378b02936fdcbbb6c2474331415462e
akowalew <ram.techen@gmail.com>
2018-01-18 19:02:51 +0100

    Updated cli.py to read algorithm type from sys.argv

--- app/cli.py
+++ app/cli.py
@@ -4,14 +4,22 @@ import sys
 from model.model import Model
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: {0} <data_dir>'.format(sys.argv[0]))
-        return 1
+	if len(sys.argv) < 3:
+		print('Usage: {0} <data_dir> <algorithm_type>'.format(sys.argv[0]))
+		return 1
 
-    dataDirectory = sys.argv[1]
-    model = Model(dataDirectory)
-    carsUsage = model.run()
-    print(carsUsage)
+	dataDirectory = sys.argv[1]
+	model = Model(dataDirectory)
+
+	try:
+		algorithmTypeStr = sys.argv[2]
+		algorithmType = Model.AlgorithmType[algorithmTypeStr]
+	except KeyError:
+		print("Unknown AlgorithmType: {0}".format(algorithmTypeStr))
+		return 1
+
+	carsUsage = model.run(algorithmType)
+	print(carsUsage)
 
 if __name__ == "__main__":
-    main()
+	main()
