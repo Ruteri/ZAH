800b638cb3ae18e0e89140c5e9e504d7e87fc334
akowalew <ram.techen@gmail.com>
2018-01-19 00:27:06 +0100

    Added parent specifier to AboutDialog

--- app/aboutdialog.py
+++ app/aboutdialog.py
@@ -2,9 +2,11 @@ from PyQt5.QtWidgets import QDialog
 from ui.ui_aboutdialog import Ui_AboutDialog
 
 class AboutDialog(QDialog):
-	def __init__(self):
-		super(AboutDialog, self).__init__()
+	def __init__(self, parent):
+		super(AboutDialog, self).__init__(parent)
 
 		# Set up the user interface from Designer.
 		self.ui = Ui_AboutDialog()
 		self.ui.setupUi(self)
+
+		
